ccc01daeb6ee2970fcbbdbb78f497c7d8acbb2a5
Jane Philipps <jane.philipps@gmail.com>
2015-05-09 14:52:37 -0700

    Adding password= method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,18 @@
+require 'bcrypt'
+
 class User < ActiveRecord::Base
+
+  BCrypt::Engine.cost = 12
+
+  attr_reader :password
+
+  def password=(unencrypted_password)
+    if unencrypted_password.nil?
+      self.password_digest = nil
+    else
+      @password = unencrypted_password
+      self.password_digest = BCrypt::Password.create(@password)
+    end
+  end
+
 end
