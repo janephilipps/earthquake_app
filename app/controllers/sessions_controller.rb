f36f70e29ac2889c2accac867de80d5d2211197d
Jane Philipps <jane.philipps@gmail.com>
2015-05-09 15:50:00 -0700

    Refactor Sessions Controller to include login method from Sessions Helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     user_params = params.require(:user)
     user = User.confirm(user_params[:email], user_params[:password])
     if user
-      session[:user_id] = user.user_id
+      login(user)
       redirect_to user_path(user.id)
     else
       redirect_to "/login"
