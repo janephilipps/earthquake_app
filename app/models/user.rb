3add75c34c6b75cd4eab70339958b13c19280925
Jane Philipps <jane.philipps@gmail.com>
2015-05-09 15:07:51 -0700

    Add password validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,20 +6,23 @@ class User < ActiveRecord::Base
 
   attr_reader :password
 
-  def password=(unencrypted_password)
-    if unencrypted_password.nil?
-      self.password_digest = nil
-    else
-      @password = unencrypted_password
-      self.password_digest = BCrypt::Password.create(@password)
-    end
-  end
+  validates_confirmation_of :password
+  validates_presence_of :password_digest
 
   def authenticate(unencrypted_password)
     if BCrypt::Password.new(password_digest) == unencrypted_password
       self
     else
       false
+    end
+  end
+
+  def password=(unencrypted_password)
+    if unencrypted_password.nil?
+      self.password_digest = nil
+    else
+      @password = unencrypted_password
+      self.password_digest = BCrypt::Password.create(@password)
     end
   end
 
