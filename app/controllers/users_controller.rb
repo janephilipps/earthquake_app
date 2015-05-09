9ea52fbb7443dc110368942487baf697cb855596
Jane Philipps <jane.philipps@gmail.com>
2015-05-09 15:20:17 -0700

    Add new, create, show, user_params, find_user methods to Users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,25 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    login(@user)
+    redirect_to_user_path(@user.id)
+  end
+
+  def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :email_confirmation, :password, :password_confirmation, :first_name, :last_name)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 
   def edit
