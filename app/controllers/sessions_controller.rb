b14e5f939bbfe350a88c75ecd48d9c226fd88fc0
Jane Philipps <jane.philipps@gmail.com>
2015-05-09 15:34:25 -0700

    Adding create method to Sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,16 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user_params = params.require(:user)
+    user = User.confirm(user_params[:email], user_params[:password])
+    if user
+      session[:user_id] = user.user_id
+      redirect_to user_path(user.id)
+    else
+      redirect_to "/login"
+    end
+  end
+  
 end
