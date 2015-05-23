29cb29bad99392d2a6f01767cbce14ae4c069a22
Jane Philipps <jane.philipps@gmail.com>
2015-05-23 09:16:33 -0700

    Add Devise views and copy sessions to users views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+
+  devise_for :users, controllers: {
+    sessions: 'sessions'
+  }
+
   root to: 'site#index'
 
   get '/about', to: 'site#about'
