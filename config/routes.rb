24f69d2cdbac69f0d4cc31d45667f4760bc25f80
Jane Philipps <jane.philipps@gmail.com>
2015-05-23 09:34:37 -0700

    Linkify signup, login, logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users, controllers: {
-    sessions: 'sessions'
-  }, path: '', path_names: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }, path: '/', path_names: {
     sign_in: 'login',
     sign_out: 'logout',
     registration: '',
