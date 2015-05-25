95fda7025680b0dd999ceac032574b9fbb57e4e4
Jane Philipps <jane.philipps@gmail.com>
2015-05-25 12:11:59 -0700

    Save checklist item in DB and display

--- app/controllers/checklist_items_controller.rb
+++ app/controllers/checklist_items_controller.rb
@@ -2,6 +2,7 @@ class ChecklistItemsController < ApplicationController
 
   def create
     @checklist_item = ChecklistItem.new(checklist_item_params)
+    @checklist_item.save
     redirect_to root_path
   end
 
